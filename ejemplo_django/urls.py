701c8d059b0e398f0ce336c5ddf1df77f45bf67b
Diego Palermo <paler.kpo@gmail.com>
2025-07-19 16:08:56 -0300

    19/07/2025

--- ejemplo_django/urls.py
+++ ejemplo_django/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
@@ -23,3 +26,7 @@ urlpatterns = [
     path('', include('mi_primer_app.urls')),
     # path('hola-mundo/', saludo),
 ]
+
+urlpatterns+= static(settings.STATIC_URL,
+                     document_root=settings.STATIC_ROOT)
+
